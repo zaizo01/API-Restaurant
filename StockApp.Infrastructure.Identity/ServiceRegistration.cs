c5029e7c4072cf0116d742b0192b098edbb640e9
zaizo01 <e.zaizortega@gamil.com>
2022-11-25 11:29:39 +0000

    * add authentication

--- StockApp.Infrastructure.Identity/ServiceRegistration.cs
+++ StockApp.Infrastructure.Identity/ServiceRegistration.cs
@@ -1,12 +1,19 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using StockApp.Core.Application.Dtos.Account;
 using StockApp.Core.Application.Interfaces.Repositories;
 using StockApp.Core.Application.Interfaces.Services;
+using StockApp.Core.Domain.Settings;
 using StockApp.Infrastructure.Identity.Entities;
 using StockApp.Infrastructure.Identity.Services;
 using StockApp.Infrastructure.Persistence.Contexts;
+using System.Text;
 
 namespace StockApp.Infrastructure.Identity
 {
@@ -41,7 +48,54 @@ namespace StockApp.Infrastructure.Identity
                 options.AccessDeniedPath = "/User/AccessDenied";
             });
 
-            services.AddAuthentication();
+            services.Configure<JWTSettings>(configuration.GetSection("JWTSettings"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.RequireHttpsMetadata = false;
+                options.SaveToken = false;
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                    ValidIssuer = configuration["JWTSettings:Issuer"],
+                    ValidAudience = configuration["JWTSettings:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWTSettings:Key"]))
+                };
+                options.Events = new JwtBearerEvents()
+                {
+                    OnAuthenticationFailed = c =>
+                    {
+                        c.NoResult();
+                        c.Response.StatusCode = 500;
+                        c.Response.ContentType = "text/plain";
+                        return c.Response.WriteAsync(c.Exception.ToString());
+                    },
+                    OnChallenge = c =>
+                    {
+                        c.HandleResponse();
+                        c.Response.StatusCode = 401;
+                        c.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new JwtResponse { HasError = true, Error = "You are not Authorized" });
+                        return c.Response.WriteAsync(result);
+                    },
+                    OnForbidden = c =>
+                    {
+                        c.Response.StatusCode = 403;
+                        c.Response.ContentType = "application/json";
+                        var result = JsonConvert.SerializeObject(new JwtResponse { HasError = true, Error = "You are not Authorized to access this resource" });
+                        return c.Response.WriteAsync(result);
+                    }
+                };
+
+            });
             #endregion
 
             #region Services
---
+++ StockApp.WebApi/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using StockApp.Core.Application.Dtos.Account;
+using StockApp.Core.Application.Interfaces.Services;
+
+namespace StockApp.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private readonly IAccountService _accountService;
+
+        public AccountController(IAccountService accountService)
+        {
+            _accountService = accountService;
+        }
+
+        [HttpPost("Authenticate")]
+        public async Task<IActionResult> AuthenticateAsync(AuthenticationRequest request)
+        {
+            return Ok(await _accountService.AuthenticateAsync(request));
+        }
+
+        [HttpPost("RegisterUser")]
+        public async Task<IActionResult> RegisterAsync(RegisterRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.RegisterBasicUserAsync(request, origin));
+        }
+
+        [HttpGet("ConfirmAccountUser")]
+        public async Task<IActionResult> RegisterAsync([FromQuery] string userId, [FromQuery] string token)
+        {
+            return Ok(await _accountService.ConfirmAccountAsync(userId, token));
+        }
+
+
+        [HttpPost("ForgotPasswordUser")]
+        public async Task<IActionResult> ForgotPasswordAsync(ForgotPasswordRequest request)
+        {
+            var origin = Request.Headers["origin"];
+            return Ok(await _accountService.ForgotPasswordAsync(request, origin));
+        }
+
+        [HttpPost("ResetPasswordUser")]
+        public async Task<IActionResult> ResetPasswordAsync(ResetPasswordRequest request)
+        {
+            return Ok(await _accountService.ResetPasswordAsync(request));
+        }
+    }
+}
+
